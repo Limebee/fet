92845550dfc6640378c32a027427989f9ed83142
LimeBee <kjeanlouis25@gmail.com>
2023-05-23 22:46:32 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+ document.querySelector('button').addEventListener('click', getDrink)
+
+function getDrink(){
+     fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+     .then(res => res.json())
+     .then(data => {
+        console.log(data.drinks[0])
+        document.querySelector('h2').innerText = data.drinks[0].strDrink
+        document.querySelector('h3').innerText = data.drinks[0].strInstructions
+        document.querySelector('img').src = data.drinks[0].strDrinkThumb
+        
+        for(let i = 1; i < 16; i++){
+            if(data.drinks[0][`strIngredient${i}`] === null){
+                break;
+            }
+
+            let ingredient = document.createElement("li")
+            ingredient.innerHTML = data.drinks[0][`strMeasure${i}`] + " " + data.drinks[0][`strIngredient${i}`]
+            
+            document.querySelector('ul').appendChild(ingredient)
+        }
+       
+     })
+       
+        
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+}
